fbf4f4515d333546f825eda6629bfe83fd1b7bde
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2024-06-08 23:49:55 +0530

    Navbar started

---
+++ app/(dashboard)/_components/sidebar/navbar.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div
+    className='flex items-center gap-x-4 p-5 bg-green-500'
+    >Navbar</div>
+  )
+}
+
+export default Navbar
